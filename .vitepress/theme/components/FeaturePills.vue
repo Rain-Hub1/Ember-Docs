839a1981924a2dae1b6c3c1bc9d23c5c2345682d
Rain-Hub1 <rainhubsla@gmail.com>
2025-11-01 23:30:57 -0300

    Create FeaturePills.vue

---
+++ .vitepress/theme/components/FeaturePills.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="feature-pills">
+    <slot></slot>
+  </div>
+</template>
+
+<style>
+.feature-pills {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 0.75rem;
+  margin: 2rem 0;
+}
+.feature-pills > div {
+  display: inline-block;
+  padding: 0.5rem 1rem;
+  border-radius: 9999px;
+  font-weight: 500;
+  font-size: 0.875rem;
+}
+</style>
