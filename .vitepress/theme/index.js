c60f4d130f5b721fe53dfd3174a0e1b3ce24b887
Rain-Hub1 <rainhubsla@gmail.com>
2025-11-01 23:29:23 -0300

    Update index.js

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -1,4 +1,12 @@
 import DefaultTheme from 'vitepress/theme'
+import HeroCard from './components/HeroCard.vue'
+import FeaturePills from './components/FeaturePills.vue'
 import './style.css'
 
-export default DefaultTheme
+export default {
+  ...DefaultTheme,
+  enhanceApp({ app }) {
+    app.component('HeroCard', HeroCard)
+    app.component('FeaturePills', FeaturePills)
+  }
+}
