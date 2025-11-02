3ff6964157d551bac9cde54891e9ff6475a68c02
Rain-Hub1 <rainhubsla@gmail.com>
2025-11-01 22:59:14 -0300

    Create MyComponent.vue

---
+++ .vitepress/theme/MyComponent.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="custom-component">
+    <p>
+      <slot>Este é um componente Vue customizado!</slot>
+    </p>
+  </div>
+</template>
+
+<style>
+.custom-component {
+  background-color: #2c2c2c;
+  border-left: 3px solid #42b883;
+  padding: 1rem 1.5rem;
+  margin: 1rem 0;
+  border-radius: 8px;
+}
+</style>
