ca37783852704ded79a04e348f2325c4e6d754b5
Rain-Hub1 <rainhubsla@gmail.com>
2025-11-01 23:10:53 -0300

    Update MyComponent.vue

--- .vitepress/theme/MyComponent.vue
+++ .vitepress/theme/MyComponent.vue
@@ -5,13 +5,3 @@
     </p>
   </div>
 </template>
-
-<style>
-.custom-component {
-  background-color: #2c2c2c;
-  border-left: 3px solid #42b883;
-  padding: 1rem 1.5rem;
-  margin: 1rem 0;
-  border-radius: 8px;
-}
-</style>
